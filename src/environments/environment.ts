fb6d3339f2c005f94a635d1337117107e7721f75
Darkzone <macair@Macbooks-MacBook-Air.local>
2021-12-13 11:39:22 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   // apiUrl : "https://192.168.1.69:5001/api/",
   apiUrl : "https://localhost:5001/api/",
   // apiUrlForImg : "https://192.168.1.69:5001",
