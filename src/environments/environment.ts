eaa729e9179fbe569d88a7f41ad22193e18189b2
Supachai Yuenyao <83196539+DarkzoneDarkzone@users.noreply.github.com>
2022-08-22 09:43:21 +0700

    Update environment.ts
    ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   // apiUrl : "https://192.168.1.69:5001/api/",
   apiUrl : "https://localhost:5001/api/",
   // apiUrlForImg : "https://192.168.1.69:5001",
