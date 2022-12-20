e39fbe3ed348f4f9d930cff271f6eb50c34e8f7e
Supachai Yuenyao <83196539+DarkzoneDarkzone@users.noreply.github.com>
2022-12-20 10:39:20 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl : "https://testrestaurant.azurewebsites.net/api/",
-  apiUrlForImg : "https://testrestaurant.azurewebsites.net/"
+  apiUrl : "https://restaurant-project-api.azurewebsites.net/api/",
+  apiUrlForImg : "https://restaurant-project-api.azurewebsites.net/"
+  //apiUrl : "https://testrestaurant.azurewebsites.net/api/",
+  //apiUrlForImg : "https://testrestaurant.azurewebsites.net/"
 };
