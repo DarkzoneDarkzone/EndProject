e8cffef9c600e467262232649cc051a8c41b0112
Supachai Yuenyao <Supachai.Y@WynnSoft-Solution.com>
2023-01-25 11:02:23 +0700

    update

--- src/app/components/income/income.component.ts
+++ src/app/components/income/income.component.ts
@@ -38,6 +38,10 @@ export class IncomeComponent implements OnInit {
   income_month: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   cash_price_month: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   bank_price_month: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  income_date: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  income_day_of_weeks: number[] = [0, 0, 0, 0, 0, 0, 0]
+  income_time: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  type_line_chart: any = "per_month"
   
   constructor(
     private callOrderApi: OrderService, 
@@ -58,7 +62,16 @@ export class IncomeComponent implements OnInit {
       this.income = data;
       for (let i = 0; i < this.income.length; i++) {
         let month = new Date(this.income[i].creationDatetime).getMonth()
+        let date = new Date(this.income[i].creationDatetime).getDate()
+        let day_of_week = new Date(this.income[i].creationDatetime).getDay()
+        let time_order = new Date(this.income[i].creationDatetime).getHours()
+        
+        this.setValueIncomeTime(time_order, this.income[i].netPrice)
+
         this.income_month[month] += this.income[i].netPrice
+        this.income_date[date-1] += this.income[i].netPrice
+        this.income_day_of_weeks[day_of_week] += this.income[i].netPrice
+
         this.totalIncome += this.income[i].netPrice
         if(this.income[i].typePayment === 'payOnsite'){
           this.cash_price_all += this.income[i].netPrice
@@ -103,6 +116,45 @@ export class IncomeComponent implements OnInit {
         this.besttypeColorArr.push(this.besttype[i].color)
       }
     })
+  }
+
+  setValueIncomeTime(hours: any, income: any){
+    if(hours === 9){
+      this.income_time[0] += income
+    }
+    if(hours === 10){
+      this.income_time[1] += income
+    }
+    if(hours === 11){
+      this.income_time[2] += income
+    }
+    if(hours === 12){
+      this.income_time[3] += income
+    }
+    if(hours === 13){
+      this.income_time[4] += income
+    }
+    if(hours === 14){
+      this.income_time[5] += income
+    }
+    if(hours === 15){
+      this.income_time[6] += income
+    }
+    if(hours === 16){
+      this.income_time[7] += income
+    }
+    if(hours === 17){
+      this.income_time[8] += income
+    }
+    if(hours === 18){
+      this.income_time[9] += income
+    }
+    if(hours === 19){
+      this.income_time[10] += income
+    }
+    if(hours === 20){
+      this.income_time[11] += income
+    }
   }
 
   pieChart() {
@@ -163,9 +215,39 @@ export class IncomeComponent implements OnInit {
       },
     };
   }
+
+  refreshChart(){
+    this.lineChart()
+  }
+
   lineChart() {
+    let label_current: any
+    let value_current: any
+    
+    const labels_month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const labels_date = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '21', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
+    const labels_day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const labels_time = ['09:00-10:00', '10:00-11:00', '11:00-12:00', '12:00-13:00', '13:00-14:00', '14:00-15:00', '15:00-16:00', '16:00-17:00', '17:00-18:00', '18:00-19:00', '19:00-20:00', '20:00-21:00'];
+
+    if(this.type_line_chart === "per_month"){
+      label_current = labels_month
+      value_current = this.income_month
+    }
+    if(this.type_line_chart === "per_date"){
+      label_current = labels_date
+      value_current = this.income_date
+    }
+    if(this.type_line_chart === "per_day"){
+      label_current = labels_day
+      value_current = this.income_day_of_weeks
+    }
+    if(this.type_line_chart === "per_time"){
+      label_current = labels_time
+      value_current = this.income_time
+    }
+
     this.lineData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      labels: label_current,
       datasets: [
         {
           label: 'Dataset 1',
@@ -173,7 +255,7 @@ export class IncomeComponent implements OnInit {
           borderColor: '#42A5F5',
           yAxisID: 'y',
           tension: .4,
-          data: this.income_month
+          data: value_current
         },
       ],
     };
